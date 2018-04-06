b829aad8889260eb2d779893b8a19c4a9e1b13bc
80buck80@gmail.com <80buck80@gmail.com>
2018-04-06 13:36:24 -0500

    .

--- sceneManagerTest/world1.js
+++ sceneManagerTest/world1.js
@@ -10,54 +10,6 @@ var dR,dB,dG,puzzle,ans,start;
 
 function World1()
 {
-<<<<<<< HEAD
-  //MAKE NODES AND PLACE THEM (X, Y, RADIUS, NODE NUMBER)
-  var node0 = new Node(200, 435, 75, 0);
-  var node1 = new Node(238, 360, 50, 1);
-  var node2 = new Node(283, 257, 50, 2);
-  var node3 = new Node(330, 167, 50, 3);
-  var node4 = new Node(588, 165, 50, 4);
-  var node5 = new Node(581, 258, 50, 5);
-  var node6 = new Node(569, 387, 50, 6);
-  var node7 = new Node(896, 387, 50, 7);
-  var node8 = new Node(862, 257, 50, 8);
-  var node9 = new Node(839, 165, 50, 9);
-  var node10 = new Node(952, 139, 75, 10);
-  var nodeArray = [node0, node1, node2,
-                   node3, node4, node5,
-                   node6, node7, node8,
-                   node9, node10];
-
-
-
-  var adjMatrix = [
-    [0, 1],
-    [1, 0, 2],
-    [2, 1, 3],
-    [3, 2, 4],
-    [4, 3, 5],
-    [5, 4, 6],
-    [6, 5, 7],
-    [7, 6, 8],
-    [8, 7, 9],
-    [9, 8, 10],
-    [10, 9]
-  ];
-
-  var player = new Player(node0, player1Piece);
-  var puzzle = new Puzzle(world1Questions);//gives the puzzle class the set of world questions
-  //var closeButton = new Button(puzzle.x + puzzle.width/2, puzzle.y + puzzle.height/9*8, "close");
-  var buttonArray = [];
-
-
-  this.draw = function()
-  {
-    //DISPLAY BOARD
-    image(this.sceneManager.worldMap1, 0, 0, width, height);
-
-    //PLACE NODES
-    for(i = 0; i < nodeArray.length; i++)
-=======
     var nodesLocation = [
         [200,423],
         [250,330],
@@ -110,7 +62,6 @@ function World1()
     puzzle.initializeQuestion(3);//tells puzzle what question to display
 
     this.draw = function()
->>>>>>> 4221bc599c87190c438581d398bbb5ed4691d9c8
     {
         //DISPLAY BOARD
         image(this.sceneManager.worldMap1, 0, 0, width, height);
