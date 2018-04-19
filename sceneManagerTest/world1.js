ced5a16115f91c3fd2eab03c23ba56fc8d456477
Aaron Bucklin <adb0026@CWSs-iMac.local>
2018-04-19 13:39:45 -0500

    reset World1 to strat at beginning

--- sceneManagerTest/world1.js
+++ sceneManagerTest/world1.js
@@ -250,7 +250,7 @@ function World1()
         player.setCollider("circle", 0,0,45);
         rocket.setCollider("circle",0,0,1);
         playstate = 0;
-        playstate = 4;
+        //playstate = 4;
 
         //set velocity to 0 to make sure its not moving.
 
